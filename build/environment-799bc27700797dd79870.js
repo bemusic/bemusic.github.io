f5a51e98bda6d07bb0e2b0359f181a35fe649eb9
Aki running on CircleCI <aki@spacet.me>
2019-09-29 20:12:13 +0000

    Update 2019-09-29T20:11:55.803Z

---
+++ build/environment-799bc27700797dd79870.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[5],{1391:function(t,n,o){"use strict";(function(t){o(1392);var n=o(157),c=o.n(n),a=o(550),s=o.n(a);t.DEBUG=c.a,t.Promise=s.a,Promise.prototype.log=function(...t){return this.tap(n=>console.log(...t.concat([n])))}}).call(this,o(35))},1742:function(t,n,o){"use strict";o.r(n);o(67),o(1391);var c=o(1393),a=o.n(c),s=o(26),i=o.n(s);window.React=i.a,a.a.attach(document.body)}}]);
+//# sourceMappingURL=environment-799bc27700797dd79870.js.map
